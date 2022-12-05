5620d4dbda73848c40a9b14609159d27abcb2de7
Robinkim93 <Robinkim93@gmail.com>
2022-12-05 22:07:21 +0900

    last

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import * as newrelic from 'newrelic';
 
 dotenv.config();
 
